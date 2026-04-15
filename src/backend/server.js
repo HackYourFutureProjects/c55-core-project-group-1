fb8645d75c2302790a752797b50219007b55172b
Salem Ba-rabuod <barboud@gmail.com>
2026-04-15 22:27:42 +0200

    search router added in server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import router from './routes/movies.js';
+
 const app = express();
 
 app.use(express.json());
+app.use('/api/movies', router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

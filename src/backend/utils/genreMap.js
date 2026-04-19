e7c924079bf5c83299768c846c7708e85ab03f2f
Muna Nasher <munanasher@gmail.com>
2026-04-19 14:46:03 +0200

    Update gernrMap.js

---
+++ src/backend/utils/genreMap.js
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////
+//GENRE_MAP: maps genre names (from user input or DB)
+// to TMDB genre IDs used in API requests 
+
+
+export  const GENRE_MAP = {
+  action: 28,
+  comedy: 35,
+  drama: 18,
+  'sci-fi': 878,
+  adventure: 12,
+  horror: 27,
+  romance: 10749,
+  thriller: 53,
+  animation: 16,
+  documentary: 99,
+};
